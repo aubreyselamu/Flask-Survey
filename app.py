066f325650dd9655df9b734756980e5d3417ddc6
Aubrey Selamu-Bell <aubrey.selamu@Aubreys-MacBook-Pro.local>
2021-11-09 15:33:21 -0500

    created start page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+from surveys import Question,Survey, satisfaction_survey
 
 app = Flask(__name__)
 
@@ -7,3 +8,11 @@ app.debug = True
 app.config['SECRET_KEY']= 'Yohanna_12'
 toolbar = DebugToolbarExtension(app)
 
+responses = []
+
+@app.route('/')
+def start_page():
+    
+    title = satisfaction_survey.title
+    instructions = satisfaction_survey.instructions
+    return render_template('base.html', title=title, instructions=instructions)
