1022909b2a01bfeca53f8b72bc163cc3d1ec7fa8
Aubrey Selamu-Bell <aubrey.selamu@Aubreys-MacBook-Pro.local>
2021-11-13 11:18:01 -0500

    referenced the session to store answers

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 from flask import Flask, render_template, session, request, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
+from werkzeug.wrappers import response
 from surveys import satisfaction_survey as survey
 
+RESPONSE_KEY = "responses"
 
 app = Flask(__name__)
-
-app.debug = True
 app.config['SECRET_KEY']= 'Yohanna_12'
 toolbar = DebugToolbarExtension(app)
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-
-responses = []
 
 @app.route('/')
 def start_page():
@@ -21,10 +19,20 @@ def start_page():
     instructions = survey.instructions
     return render_template('start_survey.html', title=title, instructions=instructions)
 
+@app.route('/begin', methods=["POST"])
+def begin():
+    '''Clear the session of responses'''
+
+    session[RESPONSE_KEY] = []
+
+    return redirect('/questions/0')
+
 @app.route('/questions/<int:qid>')
 def get_questions_page(qid):
     '''Display current question'''
-
+    
+    responses = session.get(RESPONSE_KEY)
+    
     if(responses is None):
         return redirect('/')
     
@@ -48,15 +56,17 @@ def get_choice():
     #get the response choice
     choice = request.form['answer']
     
-    #add the reponse to the response variable
+    #add the reponse to the session
+    responses = session[RESPONSE_KEY]
     responses.append(choice)
-    
+    session[RESPONSE_KEY] = responses
 
     if len(survey.questions) == len(responses):
-        return render_template('thankyou.html')
+        return redirect('/complete')
     else:
         return redirect(f'/questions/{len(responses)}')
 
 @app.route('/complete')
 def complete():
+    '''Survey complete. Completion page shown'''
     return render_template('thankyou.html')
